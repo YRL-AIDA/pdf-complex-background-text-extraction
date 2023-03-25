1a01afe2d37baf969c90271dbb276b2a0ae57706
sinkudo <mishazagorodnikov@mail.ru>
2023-03-26 00:06:14 +0800

    all chars

--- printCharImgs/main.py
+++ printCharImgs/main.py
@@ -1,41 +1,109 @@
 import os
-import string
-from pathlib import Path
-
+import shutil
 from PIL import Image, ImageFont, ImageDraw
 from fontTools.ttLib import TTFont
-import glob
 
 
 def font2png(fonts_folder, save_folder):
     img_height, img_width = 28, 28
     directories = []
-    lowerletter = list(string.ascii_lowercase)
-    for letter in lowerletter:
-        directories.append("./" + save_folder + "/" + letter)
-    for dir in directories:
-        if os.path.exists(dir):
-            for file in glob.glob(dir + "/*.png"):
-                os.remove(file)
-        else:
-            os.makedirs("./" + dir)
     fontsDir = "./" + fonts_folder + "/"
-    fontFiles = os.listdir(os.fsencode(fontsDir))
+    chars = get_all_chars_set(fontsDir)
+
+    # очистить папку
+    if os.path.isdir(save_folder):
+        shutil.rmtree(save_folder)
+    os.makedirs("./" + save_folder)
+    # создать папки
+    for char in chars:
+        name = symb2str(char)
+        os.makedirs("./" + save_folder + "/" + name)
+
+    # for char in chars:
+    #     name = symb2str(char)
+    #     directories.append("./" + save_folder + "/" + name)
+    # for dir in directories:
+    #     if os.path.exists(dir):
+    #         for file in glob.glob(dir + "/*.png"):
+    #             os.remove(file)
+    #         os.rmdir(dir)
+    # for dir in directories:
+    #     print(dir)
+    #     os.makedirs(dir)
+
     counter = 0
+    fontFiles = os.listdir(os.fsencode(fontsDir))
     for fontFile in fontFiles:
         fontName = os.fsdecode(fontFile)
-        for idx, i in enumerate(lowerletter):
+        font_chars = get_char_list_from_ttf(fontsDir + fontName)
+        # print(font_chars)
+        for char in font_chars:
             img = Image.new('L', (img_width, img_height))
             draw = ImageDraw.Draw(img)
-
             font = ImageFont.truetype(fontsDir + fontName, 16)
-            _, _, w, h = draw.textbbox((0, 0), i, font)
+            _, _, w, h = draw.textbbox((0, 0), char, font)
+            draw.text(((img_width - w) / 2, (img_height - h) / 2), char, "white", font)
 
-            draw.text(((img_width - w) / 2, (img_height - h) / 2), i, "white", font)
-            imgName = i + "_" + str(counter) + ".png"
-            img.save("./" + save_folder + "/" + i + "/" + imgName)
+            # пустое изображение
+            if img.getbbox() is None:
+                continue
+
+            # folder_name = symb2str(char)
+            # if not os.path.isdir("./" + save_folder + "/" + folder_name):
+            #     os.makedirs("./" + save_folder + "/" + folder_name)
+
+            imgName = symb2str(char) + "_" + str(counter) + ".png"
+            img.save("./" + save_folder + "/" + symb2str(char) + "/" + imgName)
         counter += 1
+    remove_empty_folders(save_folder)
+
+
+def symb2str(char: str):
+    invalid_symbols = {"/": "slash", "\\": "backslash", ":": "colon", "*": "asterisk", "?": "question",
+                       "\"": "quotation", "<": "less", ">": "more", "|": "vertical", " ": "space", ".": "dot"}
+    return invalid_symbols[char] if char in invalid_symbols else char + "_lower" if char.islower() else char + "_upper"
+
+
+def get_all_chars_set(fonts_path):
+    chars = set()
+    font_files = os.listdir(os.fsencode(fonts_path))
+    for fontFile in font_files:
+        path = fonts_path + os.fsdecode(fontFile)
+        for char in get_char_list_from_ttf(path):
+            chars.add(char)
+    return chars
+
+
+def remove_empty_folders(path):
+    folders = list(os.walk(path))[1:]
+    # print(folders)
+    for folder in folders:
+        # print(folder)
+        if not folder[2]:
+            os.rmdir(folder[0])
+
+
+def get_char_list_from_ttf(font_file):
+    f_obj = TTFont(font_file)
+    m_dict = f_obj.getBestCmap()
+    unicode_list = []
+    for key, _ in m_dict.items():
+        unicode_list.append(key)
+
+    char_list = [chr(ch_unicode) for ch_unicode in unicode_list if chr(ch_unicode).isprintable()]
+    return char_list
 
 
 font2png("fontstest", "testimgs")
 font2png("fontstrain", "trainimgs")
+
+# ŕ есть у некоторых, но нет
+# что чًтًоً
+# print("ᾒ".encode().decode('utf8').islower())
+# print("ᾚ".encode().decode('utf8').islower())
+# ὼ Ὼ
+font2png("fontsvalidation", "validationimgs")
+
+
+# os.makedirs("Ὼ")
+# os.makedirs("ὼ")
