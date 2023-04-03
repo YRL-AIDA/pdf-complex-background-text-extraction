194f26bca614fae068112f8a3e1a745b26fff8cd
sinkudo <mishazagorodnikov@mail.ru>
2023-04-03 21:50:50 +0800

    почистил

--- printCharImgs/main.py
+++ printCharImgs/main.py
@@ -3,12 +3,10 @@ import shutil
 from PIL import Image, ImageFont, ImageDraw
 from fontTools.ttLib import TTFont
 import configparser
-import string
 
 def font2png(fonts_folder, save_folder):
     img_height, img_width = 28, 28
     fontsDir = "./" + fonts_folder + "/"
-    # chars = get_all_chars_set(fontsDir)
     config = configparser.ConfigParser()
     config.read('config.ini', encoding='utf-8')
     chars = config.get('DEFAULT', 'Symbols')
@@ -21,18 +19,6 @@ def font2png(fonts_folder, save_folder):
     for char in chars:
         name = symb2str(char)
         os.makedirs("./imgs/" + save_folder + "/" + name)
-
-    # for char in chars:
-    #     name = symb2str(char)
-    #     directories.append("./" + save_folder + "/" + name)
-    # for dir in directories:
-    #     if os.path.exists(dir):
-    #         for file in glob.glob(dir + "/*.png"):
-    #             os.remove(file)
-    #         os.rmdir(dir)
-    # for dir in directories:
-    #     print(dir)
-    #     os.makedirs(dir)
 
     counter = 0
     fontFiles = os.listdir(os.fsencode(fontsDir))
@@ -61,7 +47,6 @@ def font2png(fonts_folder, save_folder):
 def symb2str(char: str):
     invalid_symbols = {"/": "slash", "\\": "backslash", ":": "colon", "*": "asterisk", "?": "question",
                        "\"": "quotation", "<": "less", ">": "more", "|": "vertical", " ": "space", ".": "dot"}
-    # return invalid_symbols[char] if char in invalid_symbols else char + "_lower" if char.islower() else char + "_upper"
     return invalid_symbols[char] if char in invalid_symbols else char + "_lower" if char.islower() else char + "_upper" if char.isupper() else char
 
 
@@ -77,9 +62,7 @@ def get_all_chars_set(fonts_path):
 
 def remove_empty_folders(path):
     folders = list(os.walk(path))[1:]
-    # print(folders)
     for folder in folders:
-        # print(folder)
         if not folder[2]:
             os.rmdir(folder[0])
 
@@ -97,24 +80,5 @@ def get_char_list_from_ttf(font_file):
 
 font2png("fontstest", "testimgs")
 font2png("fontstrain", "trainimgs")
-#
 # # что чًтًоً
 font2png("fontsvalidation", "validationimgs")
-
-
-# font = TTFont("Regular.otf")
-# print('я' in font.getGlyphSet())
-# print(list(string.ascii_lowercase + string.ascii_uppercase + str([chr(i) for i in range(ord('а'), ord('я')+1)]) +
-#            str([chr(i) for i in range(ord('А'), ord('Я')+1)])))
-
-
-# l = list(string.ascii_lowercase + string.ascii_uppercase) + [chr(i) for i in range(ord('а'), ord("я")+1)]+ [chr(i) for i in range(ord('А'), ord("Я")+1)] + [chr(i) for i in range(ord('0'), ord("9")+1)]
-# # print(list(string.ascii_lowercase + string.ascii_uppercase) + [chr(i) for i in range(ord('а'), ord("я")+1)]
-# #       + [chr(i) for i in range(ord('А'), ord("Я")+1)])
-#
-# print(','.join(l + list(string.punctuation)))
-# config = configparser.ConfigParser()
-# config.read('config.ini', encoding='utf-8')
-# l = config.get('DEFAULT', 'Symbols')
-# l = set([n.strip() for n in l])
-# print(l)
