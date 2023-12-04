8df313a42fae7a6d67f72ee89880afc74d97b82a
sinkudo <mishazagorodnikov@mail.ru>
2023-12-04 16:42:29 +0800

    b123

--- printCharImgs/scripts/train_model.py
+++ printCharImgs/scripts/train_model.py
@@ -7,6 +7,6 @@ from cnn_model import Model
 model = Model()
 model.prepare_data(char_pool=Language.Russian_and_English,
                    fonts_path="../data/fonts/fonts",
-                   data_save_path="../data/datasets/balls")
+                   data_save_path="../data/datasets/123")
 model.train()
-model.save("balls")
+model.save("123")
